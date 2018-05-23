9cc89daa608c8fa7fcc5de7a4db2aca704a3600e
Baitharu <amit.baitharu@honeywell.com>
2018-05-23 16:15:28 +0530

    Updated the title
    5/23: Updated the title - Amit

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require_once($_SERVER['DOCUMENT_ROOT']."/learningportal/inc/inc-init.php");
 <html lang="en">
 <head>
   <meta charset="utf-8">
-  <title>Learning Portal</title>
+  <title>Our Learning Portal</title>
   <?php require_once($default_url."inc/inc-html-head.php"); ?>
 </head>
 
@@ -12,7 +12,7 @@ require_once($_SERVER['DOCUMENT_ROOT']."/learningportal/inc/inc-init.php");
   <div class="container">
     <?php require_once($default_url."inc/inc-header.php"); ?>
     <div class="content">
-      <h1>Learning Portal Home</h1>
+      <h1>Our Learning Portal Home</h1>
     </div>
     <?php require_once($default_url."inc/inc-footer.php"); ?>
     <?php require_once($default_url."inc/inc-footer-scripts.php"); ?>
